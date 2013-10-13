42b07bec6bbc38b6c6a9b337f1d8e59fa30f0a11
Ron Michael Zettlemoyer <ron@zettlemoyer.com>
2013-10-13 16:09:34 -0400

    initial commit

---
+++ Knapi/Knapi.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Web;
+using Newtonsoft.Json;
+using System.Collections;
+using System.Threading;
+
+
+namespace Knapi
+{
+    public class Service
+    {
+
+
+        public class Throttle
+        {
+            public int callCount = 0;
+            public int seconds = 0;
+            public bool allowBursting = false; // not implemented; no bursting yet
+            public int wait {
+                get {
+                    if (callCount == 0) return 0;
+                    return (seconds*1000)/callCount;
+                }
+            }
+        }
+
+
+        public Throttle throttle { get; set; }
+        public string baseUrl { get; set; }
+        public Dictionary<string, string> headers { get; set; }
+
+
+        public DateTime? lastCall { get; set; }
+        public int callCount { get; set; }
+        
+        public Service()
+        {
+            Startup();
+        }
+
+        public Service(string baseUrl)
+        {
+            this.baseUrl = baseUrl;
+            Startup();
+
+        }
+        
+        private void Startup()
+        {
+            callCount = 0;
+            headers = new Dictionary<string, string>();
+            throttle = new Throttle();
+
+        }
+
+
+
+
+        public dynamic Get(string cmd)
+        {
+            string response = Http(baseUrl + cmd);
+            return JsonConvert.DeserializeObject(response);
+
+        }
+
+
+
+
+        private string Http(string url, string method = "GET", string data = "")
+        {
+            
+            if (lastCall != null && throttle.callCount > 0)
+            {
+                int wait = throttle.wait;
+                TimeSpan ts = new TimeSpan(DateTime.Now.Ticks-((DateTime)lastCall).Ticks);
+                if (ts.TotalMilliseconds < wait)
+                {
+                    Thread.Sleep(wait - Convert.ToInt32(ts.TotalMilliseconds));
+                }
+            }
+
+            callCount++;
+            lastCall = DateTime.Now;
+
+            string output = "";
+
+            //string key = "";
+            //var encKey = Convert.ToBase64String(Encoding.Default.GetBytes(key + ":"));
+            //request.Headers.Add("Authorization", "Basic " + encKey);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = method;
+
+            request.UserAgent = "Knapi 0.1";
+
+            foreach (KeyValuePair<string,string> header in headers)
+            {
+                string checkKey = header.Key.ToLower();
+
+                if (checkKey=="user-agent")
+                    request.UserAgent = header.Value;
+                else
+                    request.Headers.Add(header.Key, header.Value);
+            }
+
+
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+
+            if (data.Length > 0)
+            {
+                byte[] ba = Encoding.UTF8.GetBytes(data);
+                request.ContentLength = ba.Length;
+
+                Stream ds = request.GetRequestStream();
+                ds.Write(ba, 0, ba.Length);
+                ds.Close();
+            }
+
+
+            // check for Retry-After response header (# of seconds to wait)
+
+            output = new StreamReader(request.GetResponse().GetResponseStream()).ReadToEnd();
+
+            return output;
+
+
+        }
+
+        
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,4 +1,42 @@
-knapi
-=====
-
 Knapi API Valet: Universal REST API toolkit for .NET
+=======================================================
+
+Knapi is designed to be a simple toolkit to get you connected to any REST API
+in a matter of seconds.
+
+Features:
+
+- Trottling
+- Dynamic JSON deserialization (thanks to Json.Net)
+
+
+Quick example using AngelList:
+
+	Knapi.Service angel = new Knapi.Service("https://api.angel.co/1/");
+
+	// AngelList limits their API to 1000 requests per hour, so...
+	angel.throttle.callCount = 1000;
+	angel.throttle.seconds = 60 * 60;
+
+	// Be considerate and let the server know who you are
+	angel.headers.Add("From", "me@myserver.com");
+
+	dynamic results = angel.Get("jobs?page=" + page);
+
+	foreach (dynamic job in results.jobs)
+	{
+		Console.WriteLine(job.title);
+	}
+
+
+To do:
+
+- Allow async calls
+- Error handling
+- Handle Retry-After responses from server
+- Allow throttle bursting; custom throttle handler?
+- Built in support for Authorization encoding?
+- Built in support for cookies
+- Handle XML
+- Allow for POST
+
